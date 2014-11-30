7d3718c2af1b961b4eb9781f3ba07ede9d224845
Billy Rhoades <billyarhoades@gmail.com>
2014-11-30 15:31:05 -0600

    Added base name printing logic

--- ui/views.py
+++ ui/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
+from ui.models import Signature
 
 def index( request ):
     """Main index view with something about myself.
     """
-    return HttpResponse( "This is a page about me." )
+    r = [ ]
+    signatures = Signature.objects
+
+    if signatures.count( ) == 0:
+        r.append( "Nobody has signed my guestbook yet." )
+    else:
+        for s in signatures:
+            r.append( ''.join( s.surname, ", ", s.name ) )
+            
+    # Add form?
+    return HttpResponse( '\n'.join( r ) )
 
