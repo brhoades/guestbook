5567c2f25650b0f4661bb052268e5255dc905575
Billy Rhoades <billyarhoades@gmail.com>
2014-11-30 15:43:52 -0600

    Moved index to template

--- ui/views.py
+++ ui/views.py
@@ -1,18 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
 from ui.models import Signature
 
 def index( request ):
     """Main index view with something about myself.
     """
-    r = [ ]
+    r = { }
     signatures = Signature.objects
 
     if signatures.count( ) == 0:
-        r.append( "Nobody has signed my guestbook yet." )
+        r['there_are_no_signatures'] = True
     else:
-        for s in signatures:
-            r.append( ''.join( s.surname, ", ", s.name ) )
-            
-    # Add form?
-    return HttpResponse( '\n'.join( r ) )
+        r['signatures'] = signatures
+
+    return render( request, 'ui/index.html', r )
 
