f408f382ed449155e73e8bd4cad1b046779a852b
Billy Rhoades <billyarhoades@gmail.com>
2014-11-30 15:02:36 -0600

    Added base model

--- ui/models.py
+++ ui/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Signature( models.Model ):
+    name = models.CharField( max_length=50 )
+    surname = models.CharField( max_length=50 )
+
+    time = models.DateTimeField( 'Date Signed' )
+
+    IP = models.CharField( max_length=100 )
+    useragent = models.CharField( max_length=250 )
+
+
+
